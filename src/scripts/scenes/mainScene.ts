42e5d1a15afa56ef215bccb02e78470a3ae19467
Yannick <20306025+yandeu@users.noreply.github.com>
2019-01-30 10:11:24 +0100

    display the Phaser.VERSION

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -11,6 +11,14 @@ export default class MainScene extends Phaser.Scene {
   create() {
     new PhaserLogo(this, this.cameras.main.width / 2, 0)
     this.fpsText = new FpsText(this)
+
+    // display the Phaser.VERSION
+    this.add
+      .text(this.cameras.main.width - 15, 15, `Phaser v${Phaser.VERSION}`, {
+        color: '#000000',
+        fontSize: 24
+      })
+      .setOrigin(1, 0)
   }
 
   update() {
