2b5835fc111892831450bfd13aa3ab89e8931923
yandeu <20306025+yandeu@users.noreply.github.com>
2021-03-05 23:20:19 +0100

    Update prettier

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -16,7 +16,7 @@ export default class MainScene extends Phaser.Scene {
     this.add
       .text(this.cameras.main.width - 15, 15, `Phaser v${Phaser.VERSION}`, {
         color: '#000000',
-        fontSize: '24px',
+        fontSize: '24px'
       })
       .setOrigin(1, 0)
   }
